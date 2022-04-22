5455a0849f412bf449d314c85bbda5de4f41de67
Lonxyboi <drewthemachine22@gmail.com>
2022-04-22 15:49:02 -0400

    finished passGen

--- script.js
+++ script.js
@@ -1,60 +1,115 @@
 // Assignment code here
 
 // Special characters 
-var character = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "\:", "\;", " < ", "=", " > ", " ? ", "@", "[", "\\", "]", " ^ ", "_", "`", "{", "|", "}", "~"];
+character = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "\:", "\;", " < ", "=", " > ", " ? ", "@", "[", "\\", "]", " ^ ", "_", "`", "{", "|", "}", "~"];
 // Numeric characters
-var number = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-// Alphabetical characters lowercase
-var alphaLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-//upper case letters
-var alphaUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+number = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// Alphabetical characters
+alpha = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+// Space is for the Uppercase conversion
+space = [];
 
-
-
+var toUpper = function (x) {
+    return x.toUpperCase();
+};
+// creates a variable for uppercase conversion
+alpha2 = alpha.map(toUpper);
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 //actual password generator
 function generatePassword() {
-    var confirmLength = window.prompt("How many characters would you like your password? Please choose between 8-128");
-
-    var confirmLength = (prompt("How many characters would you like your password to contain?"));
-
-    // Loop if answer is outside the parameters 
-    while(confirmLength <= 7 || confirmLength >= 51) {
-        alert("Password length must be between 8-50 characters Try again");
-        var confirmLength = (prompt("How many characters would you like your password to contain?"));
-        } 
-  
-        // Repeat back how many charactes the user will have  
-        alert(`Your password will have ${confirmLength} characters`);
-  
-      // Determine parameters of password 
-      var confirmSpecialCharacter = confirm("Click OK to confirm if you would like to include special characters");
-      var confirmNumericCharacter = confirm("Click OK to confirm if you would like to include numeric characters");    
-      var confirmLowerCase = confirm("Click OK to confirm if you would like to include lowercase characters");
-      var confirmUpperCase = confirm("Click OK to confirm if you would like to include uppercase characters");
-        // Loop if answer is outside the parameters 
-        while(confirmUpperCase === false && confirmLowerCase === false && confirmSpecialCharacter === false && confirmNumericCharacter === false) {
-          alert("You must choose at least one parameter");
-          var confirmSpecialCharacter = confirm("Click OK to confirm if you would like to include special characters");
-          var confirmNumericCharacter = confirm("Click OK to confirm if you would like to include numeric characters");    
-          var confirmLowerCase = confirm("Click OK to confirm if you would like to include lowercase characters");
-          var confirmUpperCase = confirm("Click OK to confirm if you would like to include uppercase characters");   
-      } 
   
         // Assign an action to the password parameters
-        var passwordCharacters = []
+        var passwordCharacters;
+
+        // Asks for user input
+    enter = parseInt(prompt("How many characters would you like your password? Choose between 8 and 128"));
+    // First if statement for user validation 
+    if (!enter) {
+        alert("This needs a value");
+    } else if (enter < 8 || enter > 128) {
+        // Validates user input
+        // Start user input prompts
+        enter = parseInt(prompt("You must choose between 8 and 128"));
+
+    } else {
+        // Continues once user input is validated
+        var confirmNumber = confirm("Will this contain numbers?");
+        var confirmCharacter = confirm("Will this contain special characters?");
+        var confirmUppercase = confirm("Will this contain Uppercase letters?");
+        var confirmLowercase = confirm("Will this contain Lowercase letters?");
+    };
+
+    // Else if for 4 negative options
+    if (!confirmCharacter && !confirmNumber && !confirmUppercase && !confirmLowercase) {
+        passwordCharacters = alert("You must choose a criteria!");
+
+    }
+    // First if statement that uses user input prompts to determine passwordCharacters
+    // Else if for 4 positive options
+    else if (confirmCharacter && confirmNumber && confirmUppercase && confirmLowercase) {
+
+        passwordCharacters = character.concat(number, alpha, alpha2);
+    }
+    // Else if for 3 positive options
+    else if (confirmCharacter && confirmNumber && confirmUppercase) {
+        passwordCharacters = character.concat(number, alpha2);
+    }
+    else if (confirmCharacter && confirmNumber && confirmLowercase) {
+        passwordCharacters = character.concat(number, alpha);
+    }
+    else if (confirmCharacter && confirmLowercase && confirmUppercase) {
+        passwordCharacters = character.concat(alpha, alpha2);
+    }
+    else if (confirmNumber && confirmLowercase && confirmUppercase) {
+        passwordCharacters = number.concat(alpha, alpha2);
+    }
+    // Else if for 2 positive options 
+    else if (confirmCharacter && confirmNumber) {
+        passwordCharacters = character.concat(number);
+
+    } else if (confirmCharacter && confirmLowercase) {
+        passwordCharacters = character.concat(alpha);
+
+    } else if (confirmCharacter && confirmUppercase) {
+        passwordCharacters = character.concat(alpha2);
+    }
+    else if (confirmLowercase && confirmNumber) {
+        passwordCharacters = alpha.concat(number);
+
+    } else if (confirmLowercase && confirmUppercase) {
+        passwordCharacters = alpha.concat(alpha2);
+
+    } else if (confirmNumber && confirmUppercase) {
+        passwordCharacters = number.concat(alpha2);
+    }
+    // Else if for 1 positive option
+    else if (confirmCharacter) {
+        passwordCharacters = character;
+    }
+    else if (confirmNumber) {
+        passwordCharacters = number;
+    }
+    else if (confirmLowercase) {
+        passwordCharacters = alpha;
+    }
+    // Created space variable to fill uppercase conversion
+    else if (confirmUppercase) {
+        passwordCharacters = space.concat(alpha2);
+    };
+
+
         
-      
+        
   
         console.log(passwordCharacters)
   
         // Empty string to be filled based on for loop selecting random characters from the array
         var randomPassword = ""
         
-        for (var i = 0; i < confirmLength; i++) {
+        for (var i = 0; i < enter; i++) {
           randomPassword = randomPassword + passwordCharacters[Math.floor(Math.random() * passwordCharacters.length)];
           console.log(randomPassword)
         }
